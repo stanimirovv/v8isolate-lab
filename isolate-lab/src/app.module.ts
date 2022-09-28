86e7358bd9c21b2e1ea6a9217fbe88a7f1a50b1d
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-09-28 20:26:44 +0300

    feat(user): add user module

--- isolate-lab/src/app.module.ts
+++ isolate-lab/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IsolateModule } from './modules/isolate/isolate.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [IsolateModule],
+  imports: [IsolateModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
