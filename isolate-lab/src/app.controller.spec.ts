d1352895b0af513e51f736576275bd026ebf2bb0
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-10-02 16:24:00 +0300

    fix(tests): add missing dep

--- isolate-lab/src/app.controller.spec.ts
+++ isolate-lab/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './lib/logger/logger.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, LoggerService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
