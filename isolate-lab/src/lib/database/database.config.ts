5ed07ee3435163bb341c65a91a344b03c05a4d35
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-10-02 16:20:46 +0300

    feat(db): add path to entities

--- isolate-lab/src/lib/database/database.config.ts
+++ isolate-lab/src/lib/database/database.config.ts
@@ -3,7 +3,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const dbConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: ':memory',
-  synchronize: false,
+  dropSchema: true,
+  synchronize: true,
+  entities: [__dirname + '/../../**/*.entity.ts'],
 };
 
 export default dbConfig;
