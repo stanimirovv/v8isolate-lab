adb8c61b85e546e312f4d6d764425f073e018863
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-10-02 14:51:46 +0300

    feat: add logger example

--- isolate-lab/src/app.module.ts
+++ isolate-lab/src/app.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import dbConfig from './lib/database/database.config';
+import { LoggerService } from './lib/logger/logger.service';
 import { IsolateModule } from './modules/isolate/isolate.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dbConfig as any), IsolateModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LoggerService],
 })
 export class AppModule {}
--- isolate-lab/src/app.service.ts
+++ isolate-lab/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './lib/logger/logger.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: LoggerService) {}
+
   getHello(): string {
+    this.logger.debug('Hello world!');
     return 'Hello World!';
   }
 }
